9488b9681b291bcc9e83b04a258ca0adbcad890a
Marcos Macedo <marcosmacedo30@hotmail.com>
2025-04-12 14:38:43 -0400

    First commit

---
+++ docs/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://codetransengine.github.io',
+    base: '/',
+    output: 'static',
+    integrations: [starlight({
+        title: '🛤️ CodeTransEngine',
+        customCss: [
+            './src/styles/global.css',
+        ],
+        social: {
+            github: 'https://github.com/CodeTransEngine/CodeTransEngine',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+				autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), tailwind()],
+});
